4e8aa727312ee5601a83ea1934b6b1af730bc47c
shbodake <sayalibodake28@gmail.com>
2021-11-27 19:41:06 +0530

    Added: Program for create contact in address book system

--- src/main/java/com/blp/workshop/AddressBookMain.java
+++ src/main/java/com/blp/workshop/AddressBookMain.java
@@ -1,4 +1,10 @@
 package com.blp.workshop;
 
 public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+        Contacts contact = new Contacts("Sayali", "Bodake", "Sai Paradise Apartment", "Nashik", "Maharashtra", 422003, "9850521236", "sayalibodake28@gmail.com");
+        System.out.println(contact);
+    }
+
 }
