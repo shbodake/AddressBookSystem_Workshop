7c1f6769a8b8b50832fd6d7cd6b6df64b4e867c0
shbodake <sayalibodake28@gmail.com>
2021-11-27 19:53:51 +0530

    Added: Program for add new contact in address book system

--- src/main/java/com/blp/workshop/AddressBookMain.java
+++ src/main/java/com/blp/workshop/AddressBookMain.java
@@ -1,10 +1,43 @@
 package com.blp.workshop;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+    Contacts contacts;
+
+    private void addContacts() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The First Name  - ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter The Last Name - ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter The Address - ");
+        String address = scanner.nextLine();
+        System.out.println("Enter The City - ");
+        String city = scanner.nextLine();
+        System.out.println("Enter The State - ");
+        String state = scanner.nextLine();
+        System.out.println("Enter The Zip Code - ");
+        int zipCode = scanner.nextInt();
+        System.out.println("Enter The Mobile Number - ");
+        String number = scanner.next();
+        System.out.println("Enter The Email-id - ");
+        String email = scanner.next();
+        contacts = new Contacts(firstName, lastName, address, state, city, zipCode, number, email);
+        System.out.println("Contact Successfully Added");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
-        Contacts contact = new Contacts("Sayali", "Bodake", "Sai Paradise Apartment", "Nashik", "Maharashtra", 422003, "9850521236", "sayalibodake28@gmail.com");
-        System.out.println(contact);
+        AddressBookMain addressBook = new AddressBookMain();
+        addressBook.addContacts();
+        System.out.println(addressBook.toString());
     }
 
 }
